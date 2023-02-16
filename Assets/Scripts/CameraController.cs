30487838a731023b5aea262d4529fa862caa53ff
Kai <119766049+KaiGailitis@users.noreply.github.com>
2023-02-16 00:21:21 -0500

    Basic level layout + tileset and background art implemented

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 1f, -10f);
-    private float smoothTime = 0.25f;
-    private Vector3 velocity = Vector3.zero;
+    public float minXClamp;
+    public float maxXClamp;
 
-    [SerializeField] private Transform target;
+    public Transform player;
 
-    void Update()
+    void LateUpdate()
     {
-        Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        Vector3 cameraPosition;
+
+        cameraPosition = transform.position;
+        cameraPosition.x = Mathf.Clamp(player.transform.position.x, minXClamp, maxXClamp);
+
+        transform.position = cameraPosition;
     }
 }
